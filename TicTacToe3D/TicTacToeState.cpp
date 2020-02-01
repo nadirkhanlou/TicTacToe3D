c66e1093b09d96c72b39525dfc14bed7f1a33a2e
nadirkhanlou <39118903+nadirkhanlou@users.noreply.github.com>
2020-02-01 21:14:47 +0330

    Added TicTacToeState declarations and definitions.

--- TicTacToe3D/TicTacToeState.cpp
+++ TicTacToe3D/TicTacToeState.cpp
@@ -1,4 +1,80 @@
 #include "TicTacToeState.h"
 
-using namespace TicTacToe;
+namespace TicTacToe {
+	TicTacToeState::TicTacToeState(unsigned int size) : _n(size), _lastFill(-1)
+	{
+		this->_gameBoard = new char[_n * _n * _n];
+		for (unsigned int i = 0; i < _n * _n * _n; ++i)
+		{
+			this->_gameBoard[i] = '-';
+		}
+	}
+
+	TicTacToeState::TicTacToeState(const TicTacToeState& other)
+		: _n(other._n), _lastFill(other._lastFill)
+	{
+		this->_gameBoard = new char[_n * _n * _n];
+		for (unsigned int i = 0; i < _n * _n * _n; ++i)
+		{
+			this->_gameBoard[i] = other._gameBoard[i];
+		}
+	}
+
+	TicTacToeState::TicTacToeState(TicTacToeState&& other)
+		: _n(other._n), _lastFill(other._lastFill)
+	{
+		this->_gameBoard = other._gameBoard;
+		other._gameBoard = nullptr;
+	}
+
+	TicTacToeState& TicTacToeState::operator=(const TicTacToeState& other)
+	{
+		this->_n = other._n;
+		this->_lastFill = other._lastFill;
+		delete[] this->_gameBoard;
+		this->_gameBoard = new char[_n * _n * _n];
+		for (unsigned int i = 0; i < _n * _n * _n; ++i)
+		{
+			this->_gameBoard[i] = other._gameBoard[i];
+		}
+	}
+
+	TicTacToeState& TicTacToeState::operator=(TicTacToeState&& other)
+	{
+		this->_n = other._n;
+		this->_lastFill = other._lastFill;
+		delete[] this->_gameBoard;
+		this->_gameBoard = other._gameBoard;
+		other._gameBoard = nullptr;
+	}
+
+	char TicTacToeState::Get(unsigned int idx)
+	{
+		if (idx >= _n * _n * _n)
+		{
+			return '/0';
+		}
+		return _gameBoard[idx];
+	}
+
+	char TicTacToeState::Get(unsigned int i, unsigned int j, unsigned int k)
+	{
+		return Get(i * _n * _n + j * _n + k);
+	}
+
+	void TicTacToeState::Print()
+	{
+		std::ostream_iterator<char> outIt(std::cout, " ");
+		for (unsigned int j = 0; j < _n; ++j)
+		{
+			for (unsigned int i = 0; i < _n; i++)
+			{
+				std::copy(_gameBoard + i * _n * _n + j * _n, _gameBoard + i * _n * _n + j * _n + _n, outIt);
+				std::cout << '\t';
+			}
+			std::cout << '\n';
+		}
+	}
+
+} // namespace TicTacToe
 
--- TicTacToe3D/TicTacToeState.h
+++ TicTacToe3D/TicTacToeState.h
@@ -1,7 +1,25 @@
 #pragma once
 
-namespace TicTacToe {
-	class TicTacToeState {
+#include <iostream>
+#include <iterator>
 
+namespace TicTacToe {
+	class TicTacToeState
+	{
+	private:
+		unsigned int _n;
+		char* _gameBoard;
+		int _lastFill;
+
+	public:
+		TicTacToeState(unsigned int size);
+		TicTacToeState(const TicTacToeState& other);
+		TicTacToeState(TicTacToeState&& other);
+		TicTacToeState& operator=(const TicTacToeState& other);
+		TicTacToeState& operator=(TicTacToeState&& other);
+
+		char Get(unsigned int idx);
+		char Get(unsigned int i, unsigned int j, unsigned int k);
+		void Print();
 	}; // class TicTacToeState
 } // namespace TicTacToe
